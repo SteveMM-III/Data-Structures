67f53e78e8311ed1a0c26de81f7102659bd2ba87
Kronykus <MoreheadSteve@gmail.com>
2020-07-22 21:24:27 -0500

    forked/branched/started module1

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__( self, value ):
+        self.value = value
+        self.next = None
+
+    def get_value( self ):
+        return self.value
+
+    def get_next( self ):
+        return self.next
+
+    def set_next( self, new_next ):
+        self.next = new_next
+
+
+class LinkedList:
+    def __init__( self ):
+        self.head = None
+        self.tail = None
+    
+    def add_to_tail( self, value ):
+        # create a new node
+        new_node = Node( value )
+
+        # if list empty
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        # list not empty
+        else:
+            self.tail.set_next( new_node )
+            self.tail = new_node
+    
+    def remove_head( self ):
+        # if list empty
+        if not self.head:
+            return None
+        # head has no next
+        elif not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        # list with next
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
+
+    def contains( self, value ):
+        # if list empty
+        if not self.head:
+            return False
+        # check for value
+        else:
+            test_node = self.head
+            while test_node:
+                if test_node.get_value() == value:
+                    return True
+                test_node = test_node.get_next()
+            return False
+
+    def get_max( self ):
+        # if list empty
+        if not self.head:
+            return None
+        # check values
+        else:
+            nxt = self.head
+            mx  = 0
+
+            while nxt:
+                test = nxt.get_value()
+                if test > mx:
+                    mx = test
+                nxt = nxt.get_next()
+            return mx
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,46 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# array method
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
+        
 
     def push(self, value):
-        pass
+        self.storage.append( value )
+        self.size += 1
 
     def pop(self):
+        # check len
+        val = None
+        if self.size > 0:
+            val = self.storage[ -1 ]
+            del self.storage[ -1 ]
+            self.size -= 1
+        return val
+
+
+# LinkedList method
+import sys
+sys.path.append( '..' )
+from singly_linked_list.singly_linked_list import LinkedList
+class Stack:
+    def __init__( self ):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__( self ):
+        return self.size
+
+    def push( self, value ):
+        self.storage.add_to_tail( value )
+        self.size += 1
+
+    def pop( self ):
         pass
